2d84c64f303f1f3347199170e549ead2c386ffd8
Bárbara Marcheti Fiorin <bfiorin@psgtecnologiaaplicada.com.br>
2023-02-03 09:28:57 -0400

    adicionando menu responsivo

--- src/components/MainLayout/index.tsx
+++ src/components/MainLayout/index.tsx
@@ -1,50 +1,107 @@
-import { useState } from "react";
-import { Link } from "react-router-dom";
-import RoutesURL from "../../_shared/Routes.enum";
-import Footer from "../Footer";
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import RoutesURL from '../../_shared/Routes.enum';
+import Footer from '../Footer';
 
 interface Props {
   children: React.ReactNode;
 }
+
 const MainLayout: React.FC<Props> = ({ children }) => {
   const [open, setOpen] = useState(false);
   const [registrationPage, setRegistrationPage] = useState(false);
+  const navigate = useNavigate();
 
   const toggleOpen = () => {
     setOpen((old) => !old);
   };
 
   return (
-    <div
-      className={`flex flex-col ${
-        registrationPage ? "min-h-screen h-full lg:bg-gray-200" : "h-screen"
-      } w-full h-screen`}
-    >
-      <div className="text-white bg-stone-900 shadow-lg">
-        <div className="xl:max-w-6xl mx-auto sm:max-w-xl">
-          <div className="flex flex-row xl:px-0 md:px-10 px-6 py-2 items-center flex-wrap">
-            <ul className="hidden sm:flex px-4">
-              <div className="hidden md:flex place-content-end gap-4">
-                <Link to={RoutesURL.PROJETOS} rel="noreferrer">
-                  <li className="cursor-pointer hover:text-teal-400 transition-all border-[1px] font-medium rounded border-teal-900 py-1 px-3 ease-in-out">
-                    Projetos
-                  </li>
-                </Link>
-                <Link to={RoutesURL.SOBRE_MIM} rel="noreferrer">
-                  <li className="cursor-pointer hover:text-teal-400 transition-all border-[1px] font-medium rounded border-teal-900 py-1 px-3 ease-in-out">
-                    Sobre Mim
-                  </li>
-                </Link>
+    <>
+      <div
+        className={`flex flex-col ${
+          registrationPage ? 'min-h-screen h-full lg:bg-indigo-300' : 'h-screen'
+        } `}
+      >
+        <div
+          className={`w-full shadow-lg text-white
+          ${open ? 'bg-white text-black' : 'bg-stone-900 shadow-lg'}`}
+        >
+          <div className="xl:max-w-6xl mx-auto sm:max-w-xl">
+            <div className="flex flex-row xl:px-0 md:px-10 px-3 py-2 items-center justify-start sm:justify-center flex-wrap">
+              <ul className="hidden sm:flex px-4">
+                <div className="hidden md:flex place-content-end gap-2">
+                  <Link to={RoutesURL.PROJETOS} rel="noreferrer">
+                    <li className="cursor-pointer hover:text-indigo-300 transition-all font-medium rounded py-1 px-3 ease-in-out">
+                      Projetos
+                    </li>
+                  </Link>
+                  <Link to={RoutesURL.SOBRE_MIM} rel="noreferrer">
+                    <li className="cursor-pointer hover:text-indigo-300 transition-all font-medium rounded py-1 px-3 ease-in-out">
+                      Sobre Mim
+                    </li>
+                  </Link>
+                </div>
+              </ul>
+              <div className="md:hidden visible text-right place-content-end">
+                <div onClick={toggleOpen}>
+                  {open ? (
+                    <svg
+                      className="h-8 w-8 text-black"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M6 18L18 6M6 6l12 12"
+                      />
+                    </svg>
+                  ) : (
+                    <svg
+                      className={`h-8 w-8 text-white`}
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    >
+                      <line x1="3" y1="12" x2="21" y2="12" /> <line x1="3" y1="6" x2="21" y2="6" />{' '}
+                      <line x1="3" y1="18" x2="21" y2="18" />
+                    </svg>
+                  )}
+                </div>
               </div>
+            </div>
+            <ul
+              className={`flex flex-col bg-white text-black gap-2 p-2 text-center ${
+                open ? '' : 'hidden'
+              }`}
+            >
+              <li
+                className="cursor-pointer hover:bg-stone-100 hover:text-indigo-300 hover:font-semibold transition-all ease-in-out w-full py-5 rounded"
+                onClick={() => navigate(RoutesURL.PROJETOS)}
+              >
+                Projetos
+              </li>
+              <li
+                className="cursor-pointer hover:bg-stone-100 hover:text-indigo-300 hover:font-semibold transition-all ease-in-out w-full py-5 rounded"
+                onClick={() => navigate(RoutesURL.SOBRE_MIM)}
+              >
+                Sobre mim
+              </li>
             </ul>
           </div>
         </div>
+        <div className="grow w-full xl:max-w-6xl mx-auto xl:px-0 md:px-10 px-4 lg:mt-12 rounded-sm lg:max-w-5xl">
+          {children}
+        </div>
+        <Footer />
       </div>
-      <div className="grow w-full xl:max-w-6xl mx-auto xl:px-0 md:px-10 px-4 bg-blue-700 lg:mt-12 rounded-sm lg:max-w-5xl">
-        {children}
-      </div>
-      <Footer />
-    </div>
+    </>
   );
 };
 

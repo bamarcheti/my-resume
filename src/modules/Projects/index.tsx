ef14be29f3257c0fd58ca466f903edcdbd90daec
Bárbara Marcheti Fiorin <bfiorin@psgtecnologiaaplicada.com.br>
2023-02-04 19:33:58 -0400

    adicionando mensagem de "carregando" na página de projetos

--- src/modules/Projects/index.tsx
+++ src/modules/Projects/index.tsx
@@ -1,10 +1,11 @@
-import MainTitle from '../../components/Title/MainTitle';
-
 const Projects = () => {
   return (
     <div className="flex flex-col">
-      <div className="flex flex-wrap justify-between">
+      {/* <div className="flex flex-wrap justify-between">
         <MainTitle title="Projetos" />
+      </div> */}
+      <div className="flex justify-center items-center pt-32">
+        <p className="font-bold text-2xl text-primary">CARREGANDO...</p>
       </div>
     </div>
   );

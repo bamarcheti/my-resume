3771029090c7b208f7844fdd9e96d89dd36c60f6
Bárbara Marcheti Fiorin <bfiorin@psgtecnologiaaplicada.com.br>
2023-02-04 14:37:50 -0400

    criando categoria Academic Graduation

--- src/modules/About/components/AcademicGraduation.tsx
+++ src/modules/About/components/AcademicGraduation.tsx
@@ -1,7 +1,26 @@
+import Card from '../../../components/Card';
+import Subtitle from '../../../components/Title/Subtitle';
+
 const AcademicGraduation = () => {
   return (
     <div className="flex flex-col">
-      <div className="flex flex-wrap justify-between">Graduação</div>
+      <div className="flex flex-col justify-between items-start">
+        <Subtitle subtitle="Academic Graduation / Title" />
+      </div>
+      <div className="flex flex-col gap-10 pt-4">
+        <div className="flex flex-row gap-10 justify-center">
+          <Card
+            title="Insted / Graduation"
+            period="FEBRUARY OF 2021 - JULY OF 2023"
+            description="Systems Analysis and Development Technology (TADS)"
+          />
+          <Card
+            title="Colégio Nova escola / High School"
+            period="FEBRUARY OF 2018 - DECEMBER OF 2020"
+            description="High School"
+          />
+        </div>
+      </div>
     </div>
   );
 };

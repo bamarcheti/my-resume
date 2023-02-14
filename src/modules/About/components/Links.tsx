c8c7926afb619f86a1d0f2094ca7454b58e87a09
Bárbara Marcheti Fiorin <bfiorin@psgtecnologiaaplicada.com.br>
2023-02-13 21:23:51 -0400

    adicionando svg email e estilização dos links

--- src/modules/About/components/Links.tsx
+++ src/modules/About/components/Links.tsx
@@ -1,5 +1,6 @@
 import discord from '../../../assets/discord.svg';
 import globe from '../../../assets/globe.svg';
+import gmail from '../../../assets/gmail.svg';
 import instagram from '../../../assets/instagram.svg';
 import linkedin from '../../../assets/linkedin.svg';
 import Subtitle from '../../../components/Title/Subtitle';
@@ -13,16 +14,39 @@ const Links = () => {
       <div className="flex flex-col sm:pt-4 pt-0">
         <div className="flex flex-row sm:flex-row gap-3 justify-start">
           <a href="http://bamarcheti.github.io/Portfolio/">
-            <img src={globe} className="globe" alt="globe" />
+            <img
+              src={globe}
+              className="transition ease-in-out hover:-translate-1 hover:scale-110"
+              alt="globe"
+            />
           </a>
           <a href="https://discord.com/channels/@ba_marcheti#3824">
-            <img src={discord} className="discord" alt="discord" />
+            <img
+              src={discord}
+              className="transition ease-in-out hover:-translate-1 hover:scale-110"
+              alt="discord"
+            />
           </a>
           <a href="https://www.instagram.com/ba_marcheti/">
-            <img src={instagram} className="instagram" alt="instagram" />
+            <img
+              src={instagram}
+              className="transition ease-in-out hover:-translate-1 hover:scale-110"
+              alt="instagram"
+            />
           </a>
           <a href="https://www.linkedin.com/in/barbara-marcheti-fiorin/">
-            <img src={linkedin} className="linkedin" alt="linkedin" />
+            <img
+              src={linkedin}
+              className="transition ease-in-out hover:-translate-1 hover:scale-110"
+              alt="linkedin"
+            />
+          </a>
+          <a href="mailto:bmarchetifiorin@gmail.com">
+            <img
+              src={gmail}
+              className="transition ease-in-out hover:-translate-1 hover:scale-110"
+              alt="gmail"
+            />
           </a>
         </div>
       </div>

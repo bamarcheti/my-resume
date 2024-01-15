b3cce34f7bb7f8101aca5fec788a4ff8da22aa4f
Bárbara Marcheti Fiorin <bfiorin@psgtecnologiaaplicada.com.br>
2024-01-15 19:29:25 -0400

    feat: Adicionando ícones da lib lucide

--- src/modules/About/components/Links.tsx
+++ src/modules/About/components/Links.tsx
@@ -1,54 +1,26 @@
-import discord from '../../../assets/discord.svg';
-import globe from '../../../assets/globe.svg';
-import gmail from '../../../assets/gmail.svg';
-import instagram from '../../../assets/instagram.svg';
-import linkedin from '../../../assets/linkedin.svg';
+import { Github, Instagram, Linkedin, Mail } from 'lucide-react';
 import Subtitle from '../../../components/Title/Subtitle';
 
 const Links = () => {
+  const iconStyle =
+    'text-whitePrimary dark:text-secondary w-7 h-7 transition ease-in-out hover:opacity-80 hover:-translate-1 hover:scale-110';
+
   return (
     <div className="flex flex-col">
-      <div className="flex flex-col justify-between items-start">
-        <Subtitle subtitle="Links" />
-      </div>
-      <div className="flex flex-col sm:pt-4 pt-0">
-        <div className="flex flex-row sm:flex-row gap-3 justify-start">
-          <a href="http://bamarcheti.github.io/Portfolio/">
-            <img
-              src={globe}
-              className="transition ease-in-out hover:-translate-1 hover:scale-110"
-              alt="globe"
-            />
-          </a>
-          <a href="https://discord.com/channels/@ba_marcheti">
-            <img
-              src={discord}
-              className="transition ease-in-out hover:-translate-1 hover:scale-110"
-              alt="discord"
-            />
-          </a>
-          <a href="https://www.instagram.com/ba_marcheti/">
-            <img
-              src={instagram}
-              className="transition ease-in-out hover:-translate-1 hover:scale-110"
-              alt="instagram"
-            />
-          </a>
-          <a href="https://www.linkedin.com/in/barbara-marcheti-fiorin/">
-            <img
-              src={linkedin}
-              className="transition ease-in-out hover:-translate-1 hover:scale-110"
-              alt="linkedin"
-            />
-          </a>
-          <a href="mailto:bmarchetifiorin@gmail.com">
-            <img
-              src={gmail}
-              className="transition ease-in-out hover:-translate-1 hover:scale-110"
-              alt="gmail"
-            />
-          </a>
-        </div>
+      <Subtitle subtitle="Links" />
+      <div className="flex flex-row sm:flex-row gap-3 justify-start sm:pt-4 pt-0">
+        <a href="http://bamarcheti.github.io/Portfolio/">
+          <Github className={iconStyle} />
+        </a>
+        <a href="https://www.instagram.com/ba_marcheti/">
+          <Instagram className={iconStyle} />
+        </a>
+        <a href="https://www.linkedin.com/in/barbara-marcheti-fiorin/">
+          <Linkedin className={iconStyle} />
+        </a>
+        <a href="mailto:bmarchetifiorin@gmail.com">
+          <Mail className={iconStyle} />
+        </a>
       </div>
     </div>
   );

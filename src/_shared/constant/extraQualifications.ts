4bb3d94e117099ea6b4c7d092e1addcf84343cb9
Bárbara Marcheti Fiorin <bfiorin@psgtecnologiaaplicada.com.br>
2024-01-26 21:36:34 -0400

    fix: Alterando sessão de Qualificações Extras para algo mais genérico e traduzindo título

---
+++ src/_shared/constant/extraQualifications.ts
@@ -0,0 +1,60 @@
+export interface ExtraQualificationsAction {
+  title: string;
+  period: string;
+  description: string;
+}
+
+export const extraQualifications: ExtraQualificationsAction[] = [
+  {
+    title: 'SEBREA',
+    period: 'SETEMBRO DE 2023',
+    description: 'Startup Day - Modelando e imprimindo o seu primeiro objeto 3D'
+  },
+  {
+    title: 'COSACO',
+    period: 'ABRIL DE 2023',
+    description:
+      'Artigo - Analisando a Fragilidade de Códigos Cifrados por Criptoanálise: estudo de caso com o Código de César'
+  },
+  {
+    title: 'COSACO',
+    period: 'ABRIL DE 2023',
+    description:
+      'Artigo - Utilização de Inteligência Artificial na transmissão de mensagens baseadas nas emoções a partir da interpretação do comportamento humano'
+  },
+  {
+    title: 'COSACO',
+    period: 'ABRIL DE 2023',
+    description: 'Artigo - Pet For You: Aplicativo de adoção de animais para Campo Grande/MS'
+  },
+  {
+    title: 'EBAC',
+    period: 'SETEMBRO DE 2022',
+    description: 'Curso - Crie “O” Case do seu Portfólio em 7 dias'
+  },
+  {
+    title: 'EBAC',
+    period: 'JULHO DE 2022',
+    description: 'Curso - Seu Começo de Carreira em Programação'
+  },
+  {
+    title: 'IFMS',
+    period: 'MARÇO DE 2022 - JUNHO DE 2022',
+    description: 'Curso - Introdução à Inteligência Artificial'
+  },
+  {
+    title: 'OAB',
+    period: 'MARÇO DE 2022',
+    description: 'Curso - Produtividade e Alta Performance'
+  },
+  {
+    title: 'CCAA / Nível 9',
+    period: '2013 - 2021, CCAA Joaquim Murtinho',
+    description: 'Inglês Avançado'
+  },
+  {
+    title: 'KUMON',
+    period: '2013 - 2016, Escola Carandá Bosque',
+    description: 'Matemática'
+  }
+];

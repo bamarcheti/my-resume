1ebf05bf8d0c11ba4fef98b65ab46984f6f15e90
Bárbara Marcheti Fiorin <bfiorin@psgtecnologiaaplicada.com.br>
2024-01-29 17:04:29 -0400

    fix: Atualizando texto de Experiencias

--- src/_shared/constant/experience.ts
+++ src/_shared/constant/experience.ts
@@ -1,23 +1,20 @@
 export const experiencePT = [
-  'Atuo nos projetos internos da empresa em que trabalho (Holerite, Carreiras, Site da empresa);',
-  'Utilizo ferramentas como React.js, Tailwind, TypeScript e Next.js diariamente em todas as frentes do projeto;',
-  'Esses projetos incluem: responsividade para desktop, tablet e mobile; integração com API; utilização do react-router-dom; além de ter arquitetura limpa e código limpo;',
-  'Convivo diariamente com metodologias ágeis como Kanban e Xp (utilizamos Scrum, daily e plannings para a gestão de nossa equipe);',
-  'Tenho um relacionamento diário e direto com minha equipe (durante o expediente, estamos em chamada fazendo nossas demandas e, caso necessário, tiramos dúvidas uns com os outros para que possamos solucionar nossos problemas).'
+  'Atuo nos projetos internos da empresa como: Sistema de Gestão de Pessoas e Site da empresa;',
+  'Utilizo as ferramentas: React.js, Next.js, TypeScript, Tailwind, Prettier e ESLint;',
+  'Estes projetos contém responsividade para desktop, tablet e mobile; integração com APIs; utilização do react-router-dom; além de ter arquitetura e código limpo;',
+  'Faço uso diariamente de metodologias ágeis com Scrum e Xp.'
 ];
 
 export const experienceEN = [
-  'I work on internal projects within the company where I am employed (Holerite, Careers, Company website);',
-  'I use tools like React.js, Tailwind, TypeScript, and Next.js daily in all aspects of the project;',
-  'These projects involve: responsiveness for desktop, tablet, and mobile; API integration; use of react-router-dom; in addition to having clean architecture and clean code;',
-  'I interact daily with agile methodologies such as Kanban and XP (we use Scrum, daily meetings, and planning for team management);',
-  'I have a daily and direct relationship with my team (during working hours, we are on calls discussing our tasks, and if necessary, we clarify doubts with each other to solve our problems).'
+  "I work on internal company projects, including the People Management System and the company's website;",
+  'I use tools such as React.js, Next.js, TypeScript, Tailwind, Prettier, and ESLint;',
+  'These projects feature responsiveness for desktop, tablet, and mobile; API integration; use of react-router-dom; as well as clean architecture and code;',
+  'I daily employ agile methodologies like Scrum and XP.'
 ];
 
 export const experienceES = [
-  'Trabajo en proyectos internos de la empresa en la que estoy empleado (Holerite, Carreras, Sitio web de la empresa);',
-  'Utilizo herramientas como React.js, Tailwind, TypeScript y Next.js diariamente en todos los aspectos del proyecto;',
-  'Estos proyectos incluyen: responsividad para escritorio, tablet y móvil; integración con API; uso de react-router-dom; además de contar con arquitectura limpia y código limpio;',
-  'Convivo diariamente con metodologías ágiles como Kanban y XP (utilizamos Scrum, reuniones diarias y planificación para la gestión de nuestro equipo);',
-  'Tengo una relación diaria y directa con mi equipo (durante el horario laboral, estamos en llamadas discutiendo nuestras tareas y, si es necesario, aclaramos dudas entre nosotros para resolver nuestros problemas).'
+  'Trabajo en proyectos internos de la empresa, incluyendo el Sistema de Gestión de Personas y el sitio web de la empresa;',
+  'Utilizo herramientas como React.js, Next.js, TypeScript, Tailwind, Prettier y ESLint;',
+  'Estos proyectos cuentan con responsividad para escritorio, tablet y móvil; integración con APIs; uso de react-router-dom; además de tener arquitectura y código limpio;',
+  'Hago uso diario de metodologías ágiles como Scrum y XP.'
 ];
--- src/modules/About/components/ExtraQualifications.tsx
+++ src/modules/About/components/ExtraQualifications.tsx
@@ -1,5 +1,6 @@
 import { useTranslation } from 'react-i18next';
 import { extraQualifications } from '../../../_shared/constant/extraQualifications';
+import Card from '../../../components/Card';
 import Subtitle from '../../../components/Subtitle';
 
 const ExtraQualifications = () => {
@@ -13,11 +14,11 @@ const ExtraQualifications = () => {
       <div className="grid md:grid-cols-3 grid-cols-1 sm:pt-4 pt-0 gap-5 sm:gap-10 w-full xl:gap-16 lg:gap-14 justify-center">
         {extraQualifications.map((extraQualification, index) => (
           <div key={index}>
-            {/* <Card
+            <Card
               title={extraQualification.title}
               period={extraQualification.period}
               description={extraQualification.description}
-            /> */}
+            />
           </div>
         ))}
       </div>

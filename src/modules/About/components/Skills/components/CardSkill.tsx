cd18b6a1a3ee51713642e38d54217b27cd36b75d
Bárbara Marcheti Fiorin <bfiorin@psgtecnologiaaplicada.com.br>
2024-02-02 18:33:35 -0400

    feat: Adicionando cursor-default no componente cardSkill

--- src/modules/About/components/Skills/components/CardSkill.tsx
+++ src/modules/About/components/Skills/components/CardSkill.tsx
@@ -10,7 +10,7 @@ const CardSkill = ({ skills }: Props) => {
       {skills.map((skill, index) => (
         <p
           key={index}
-          className="font-semibold text-xs text-white bg-secondary p-3 rounded-2xl transition ease-in-out hover:scale-105 hover:text-default"
+          className="cursor-default font-semibold text-xs text-white bg-secondary p-3 rounded-2xl transition ease-in-out hover:scale-105 hover:text-default"
         >
           {skill.name}
         </p>

1d13e86dc31a6b8dc19449d6f3d9d496b4117f68
Bárbara Marcheti Fiorin <bfiorin@psgtecnologiaaplicada.com.br>
2024-02-17 16:12:41 -0400

    delete: Deletando arquivos não utilizados

--- src/dictionaries/default-dictionaries.ts
+++
@@ -1,9 +0,0 @@
-import { enUS } from '../locales/en/translation';
-import { esEs } from '../locales/es/translation';
-import { ptBR } from '../locales/pt/translation';
-
-export const defaultDictionary = {
-  'pt-BR': ptBR,
-  'en-US': enUS,
-  'es-ES': esEs
-};

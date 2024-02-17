44b2640cbdda47a894708fa1d33468710349e906
Bárbara Marcheti Fiorin <bfiorin@psgtecnologiaaplicada.com.br>
2024-02-17 15:46:27 -0400

    fix: Corrigido link do github

--- src/modules/About/components/Links.tsx
+++ src/modules/About/components/Links.tsx
@@ -1,12 +1,12 @@
 import { Github, Instagram, Linkedin, Mail } from 'lucide-react';
 
-const Links = () => {
+export const Links = () => {
   const iconStyle =
     'text-whitePrimary dark:text-secondary w-6 h-6 transition ease-in-out hover:opacity-80 hover:-translate-1 hover:scale-110';
 
   return (
     <div className="flex flex-row gap-2">
-      <a href="https://github.com/Bamarcheti" title="Bamarcheti">
+      <a href="https://github.com/bamarcheti" title="bamarcheti">
         <Github className={iconStyle} />
       </a>
       <a href="https://www.instagram.com/ba_marcheti/" title="ba_marcheti">
@@ -24,5 +24,3 @@ const Links = () => {
     </div>
   );
 };
-
-export default Links;
